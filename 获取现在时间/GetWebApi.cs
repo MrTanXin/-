f7098415f1d3b6de21130eb476f5b2091f70ff8f
谈 欣 <tanx990512@outlook.com>
2019-05-04 16:32:27 +0800

    添加项目文件。

---
+++ 获取现在时间/GetWebApi.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace 获取现在时间
+{
+    class GetWebApi
+    {
+        private string content;
+
+        public string GetContent() => content;
+
+        public GetWebApi(string url)
+        {
+            content = Get(url);
+        }
+
+        private static string Get(string url)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            using (HttpWebResponse webResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                if (webResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    using (Stream stream = webResponse.GetResponseStream())
+                    {
+                        using (StreamReader sr = new StreamReader(stream))
+                        {
+                            string str = sr.ReadToEnd();
+                            return str;
+                        }
+                    }
+
+                }
+                else
+                {
+                    return webResponse.StatusCode.ToString();
+                }
+            }
+        }
+    }
+
+}
---
+++ 获取现在时间/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace 获取现在时间
+{
+    class Program
+    {
+        public static void ToTime(long JstimeStamp)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            DateTime dateTime = startTime.AddMilliseconds(JstimeStamp);
+            Console.WriteLine(dateTime.ToString("yyyy/MM/dd HH:mm:ss"));
+        }
+
+        static void Main(string[] args)
+        {
+            GetWebApi getWebApi = new GetWebApi("http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp");
+
+            string JsonText = getWebApi.GetContent();
+
+            JsonText = JsonText.Replace("[\"SUCCESS::接口调用成功\"]", "\"OK\"");
+            JsonText = JsonText.Replace(":{\"t\":\"", ":\"");
+            JsonText = JsonText.Replace("}}", "}");
+
+            //Console.WriteLine(JsonText);
+
+            RootObject rootObject = JsonConvert.DeserializeObject<RootObject>(JsonText);
+
+            long TimeStamp = long.Parse(rootObject.data);
+            ToTime( TimeStamp);
+        }
+    }
+
+    public class RootObject
+    {
+        public string api;
+        public string v;
+        public string ret;
+        public string data;//time Stamp
+    }
+
+}
